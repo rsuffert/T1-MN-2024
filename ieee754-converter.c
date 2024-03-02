796b934f4ed674662b96888b257c2373188b918b
Ricardo Süffert <ricardobsuffert@gmail.com>
2024-03-02 00:05:52 -0300

    add IEEE-754 error flags & update README

--- ieee754-converter.c
+++ ieee754-converter.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fenv.h>
 
 int main(int argc, char *argv[]) {
     if (argc != 4) 
@@ -11,6 +12,8 @@ int main(int argc, char *argv[]) {
     char op    = argv[2][0];
     float val1 = strtof(argv[1], NULL);
     float val2 = strtof(argv[3], NULL);
+
+    feclearexcept(FE_ALL_EXCEPT); // clear all IEEE-754 exceptions before performing operation
 
     float res;
     switch(op)
@@ -24,7 +27,13 @@ int main(int argc, char *argv[]) {
             return 1;
     }
 
-    printf("Result: %f\n", res);
+    printf("\nResult: %f %c %f = %f\n\n", val1, op, val2, res);
+    printf("IEEE-754 error flags:\n");
+    printf("\tFE_DIVBYZERO = %d\n", fetestexcept(FE_DIVBYZERO)==0? 0 : 1);
+    printf("\tFE_INEXACT   = %d\n", fetestexcept(FE_INEXACT)==0?   0 : 1);
+    printf("\tFE_INVALID   = %d\n", fetestexcept(FE_INVALID)==0?   0 : 1);
+    printf("\tFE_OVERFLOW  = %d\n", fetestexcept(FE_OVERFLOW)==0?  0 : 1);
+    printf("\tFE_UNDERFLOW = %d\n", fetestexcept(FE_UNDERFLOW)==0? 0 : 1);
 
     return 0;
 }
